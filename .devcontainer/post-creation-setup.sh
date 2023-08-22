9f60cbf1ee440fa747b0bde54645a0cd9f720ce3
BigSpaceships <bigspaceships56@gmail.com>
2023-08-22 10:03:05 -0400

    Update post-creation-setup.sh

--- .devcontainer/post-creation-setup.sh
+++ .devcontainer/post-creation-setup.sh
@@ -3,7 +3,6 @@ gh auth setup-git
 
 ## configure neovim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 ln -s /workspaces/neovim-config/nvim $HOME/.config/nvim
 
