b298958fc937d3c8fb394f7aedb34362c7083e14
BigSpaceships <bigspaceships56@gmail.com>
2023-08-23 09:28:01 -0400

    Update init.lua

--- nvim/lua/bigspaceships/init.lua
+++ nvim/lua/bigspaceships/init.lua
@@ -6,13 +6,13 @@ else
     fullMode = true
     require("bigspaceships.remap")
     require("bigspaceships.set")
+
+    require("tokyonight").setup({
+        transparent = true
+    })
+
+    vim.cmd("colorscheme tokyonight")
+
+    require("bigspaceships.remap")
+    require("bigspaceships.set")
 end
-
-require("tokyonight").setup({
-    transparent = true
-})
-
-vim.cmd("colorscheme tokyonight")
-
-require("bigspaceships.remap")
-require("bigspaceships.set")
