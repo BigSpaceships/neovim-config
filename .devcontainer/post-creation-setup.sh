44569e33c0f13a3e30f372f59c0a5405c6e8db6d
BigSpaceships <bigspaceships56@gmail.com>
2023-08-23 15:01:17 -0400

    CHORE: update post creation script again
    Revert neovim setup command and remove tmux config option

--- .devcontainer/post-creation-setup.sh
+++ .devcontainer/post-creation-setup.sh
@@ -3,10 +3,6 @@ gh auth setup-git
 
 ## configure neovim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
-
 ln -s /workspaces/neovim-config/nvim $HOME/.config/nvim
 
-nvim --headless +PackerSync
-
-## configure tmux
-ln -s ~/.config/.tmux.conf ~/.tmux.conf
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
