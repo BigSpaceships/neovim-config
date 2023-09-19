93b2a2d2f1e1f130c016ec67ab065687555d13eb
BigSpaceships <bigspaceships56@gmail.com>
2023-09-19 14:35:07 -0400

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -17,3 +17,5 @@ cp -R /root/.oh-my-zsh /home/$USERNAME
 cp /root/.zshrc /home/$USERNAME
 sed -i -e "s/\/root\/.oh-my-zsh/\/home\/$USERNAME\/.oh-my-zsh/g" /home/$USERNAME/.zshrc
 chown -R $USER_UID:$USER_GID /home/$USERNAME/.oh-my-zsh /home/$USERNAME/.zshrc
+
+cp $containerWorkspaceFolder/.devcontainer/after-creation.sh $HOME/
