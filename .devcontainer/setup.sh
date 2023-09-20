25ad7930adee2d67aaaa5f2255293c84aea897fe
BigSpaceships <bigspaceships56@gmail.com>
2023-09-20 11:32:09 -0400

    FIX: I lied that was important

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -41,3 +41,5 @@ else
 fi
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
